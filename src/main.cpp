335b81950283e06029a20fecffa8f95b655f7414
LeFlou <leflou@gmx.com>
2016-01-15 21:17:45 +0100

    Update main.cpp to test insert method

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,13 @@
 
 int main()
 {
-    LRUCache<int, std::string, 5> myLRU;
+    LRUCache<int, std::string, 3> myLRU;
 
-    myLRU.insert(std::make_pair<int, std::string>(42, "lol"));
+    myLRU.insert(std::make_pair<int, std::string>(1, "1"));
+    myLRU.insert(std::make_pair<int, std::string>(2, "2"));
+    myLRU.insert(std::make_pair<int, std::string>(3, "3"));
+    myLRU.insert(std::make_pair<int, std::string>(4, "4"));
+
     auto myString = myLRU[42];
 
     std::cout << myString << std::endl;
