51888b0da389761f718b7c99ea69ca6be5d2881f
LeFlou <leflou@gmx.com>
2016-01-15 23:25:40 +0100

    LRUCache operator[] implementation

--- headers/LRUCache.hxx
+++ headers/LRUCache.hxx
@@ -14,11 +14,11 @@ public:
     using value_iterator = decltype(*values_.begin());
     using value_type = std::pair<const _Kty, _Ty>;
     using mapping_type = std::unordered_map<_Kty, value_iterator>;
-    using iterator_type = typename mapping_type::iterator;
+    using mapping_iterator = typename mapping_type::iterator;
 
 private:
     mapping_type mapping_;
-    iterator_type oldest_ {};
+    mapping_iterator oldest_ {};
 
 public:
     LRUCache() = default;
@@ -28,7 +28,7 @@ public:
     LRUCache(LRUCache&&) = default;
     LRUCache& operator=(LRUCache&&) = default;
 
-    bool insert(const value_type& value)
+    std::pair<mapping_iterator, bool> insert(const value_type& value)
     {
         static_assert(_Size > 0, "Size must be greater than zero.");
         if (values_.size() == _Size)
@@ -42,7 +42,7 @@ public:
         }
 
         values_.push_back(value.second);
-        mapping_.insert({ value.first, values_.back() });
+        auto result = mapping_.insert({ value.first, values_.back() });
 
         assert(values_.size() == mapping_.size());
         if (values_.size() == 1)
@@ -50,7 +50,7 @@ public:
             oldest_ = mapping_.begin();
         }
 
-        return true;
+        return std::make_pair(result.first, true);
     }
 
     _Ty& operator[](const _Kty& key)
@@ -60,10 +60,8 @@ public:
         {
             return it->second;
         }
-
-        // TODO: remove dummy and brace initialize
-        static _Ty dummy{};
-        return dummy;
+        auto result = insert({ key , _Ty{} });
+        return result.first->second;
     }
 
     size_t size() const
