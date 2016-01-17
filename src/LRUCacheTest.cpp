6daa4e68083baab68f037d673fb69a5820467f37
LeFlou <leflou@gmx.com>
2016-01-17 20:04:03 +0100

    Add unit tests covering operator[] and insert

---
+++ src/LRUCacheTest.cpp
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include "LRUCache.hxx"
+
+TEST(LRUCache, InsertPair)
+{
+    LRUCache<int, std::string, 3> cache;
+
+    auto pairResult = cache.insert(std::make_pair<int, std::string>(1, "1"));
+    EXPECT_EQ(std::string("1"), pairResult.first->second);
+    EXPECT_TRUE(pairResult.second);
+    EXPECT_EQ(1, cache.size());
+
+    pairResult = cache.insert(std::make_pair<int, std::string>(2, "2"));
+    EXPECT_EQ(std::string("2"), pairResult.first->second);
+    EXPECT_TRUE(pairResult.second);
+    EXPECT_EQ(2, cache.size());
+
+    pairResult = cache.insert(std::make_pair<int, std::string>(3, "3"));
+    EXPECT_EQ(std::string("3"), pairResult.first->second);
+    EXPECT_TRUE(pairResult.second);
+    EXPECT_EQ(3, cache.size());
+
+    pairResult = cache.insert(std::make_pair<int, std::string>(4, "4"));
+    EXPECT_EQ(std::string("4"), pairResult.first->second);
+    EXPECT_TRUE(pairResult.second);
+    EXPECT_EQ(3, cache.size());
+}
+
+TEST(LRUCache, BracketOperator)
+{
+    LRUCache<int, std::string, 5> cache;
+
+    cache.insert(std::make_pair<int, std::string>(1, "1"));
+    auto& value = cache[1];
+    EXPECT_EQ(std::string("1"), value);
+}
